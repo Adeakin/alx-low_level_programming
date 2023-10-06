b8bc9e0d6c0edd51ea752328f1b68a998319bfa8
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-05 20:11:09 -0700

    prints exactly "Programming is like

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * printf not allowed
+ * using puts instead
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
