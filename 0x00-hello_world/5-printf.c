d0f49a742bbc5ab0ac85fcac7d2fcd172e06aa7f
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-05 20:40:50 -0700

    program that prints exactly with proper grammar...

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,17 @@
-#include <studio.h>
-#include <studio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * main - entry point
  * puts is not allowed
  * use printf
- * return 0
+ * Return:0
  */
 
-int main
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
