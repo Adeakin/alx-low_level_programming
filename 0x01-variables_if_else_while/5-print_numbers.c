6ddba6f1fa7847250bf67a593b985bb70a815037
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-06 20:18:57 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - to print all single numbers of base 10
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	printf("%d", a);
+	printf("\n");
+	return (0);
+}
