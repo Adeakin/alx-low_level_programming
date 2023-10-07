8ff1c0b92f63ac02f31e90c38337a88a9acdb856
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-06 17:56:23 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - generates random numbers,delcares + OR -
+ *
+ * Return: 1, if succesful
+ */
+
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,9 @@
+#Variables if, else, while
+
+In this project, i learntthe following:
+*arithemtric operator, logical operator (boolean operators) and relational operator.
+*how to assign values to variables
+*how to declare and print varables.
+*how to use the if, if ... else statements.
+*how to use the while loop
+
