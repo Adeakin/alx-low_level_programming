c474ace9a18ecf8f2f3972403f342fb03b84fd87
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-12 19:30:18 -0700

    task 0

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -4,15 +4,7 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-void positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-}
+
 int main(void)
 {
         int i;
