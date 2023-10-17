2d67ffcbf6e9dc7896557a97cd36723ee4e2c40a
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-17 11:47:23 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
 			if (y >= 10)
 				putchar(y / 10 + '0');
