4fad84d6548e1d27141b38f089bab36c13f580c8
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-17 23:32:27 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_number(98);
+	putchar('\n');
+	print_number(402);
+	putchar('\n');
+	print_number(1024);
+	putchar('\n');
+	print_number(0);
+	putchar('\n');
+	print_number(-98);
+	putchar('\n');
+	return (0);
+}
