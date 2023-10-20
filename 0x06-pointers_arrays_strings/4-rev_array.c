39969c27fb7edc00a3423efca73f93f222a13e62
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-19 22:11:40 -0700

    task 6

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,8 +8,8 @@
 void reverse_array(int *a, int n)
 {
 	int i, b, ray;
-	
-	b = n -1;
+
+	b = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,16 +4,18 @@
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @s: string to modify
  *
- * @Return: the resulting string
+ * Return: the resulting string
  */
+
 char *string_toupper(char *s)
+
 {
 	int b;
 
 	for (b = 0; s[b] != '\0'; b++)
 	{
 		if (s[b] >= 'a' && s[b] <= 'z')
-			s[b] = s[b] -32;
+			s[b] = s[b] - 32;
 	}
 
 	return (s);
