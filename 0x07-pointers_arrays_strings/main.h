9cbe39049aa3f05f9076041dc1d5a99e940b609d
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-23 21:07:33 -0700

    task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-#ifndef MAIN_FILE
-#define MAIN_FILE
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
