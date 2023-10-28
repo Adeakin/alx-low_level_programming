df88599a5f1dfd8fccf96b1ac809e668c7e611c5
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-27 21:30:17 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, len, d, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	len = 0;
+	d = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (a < len && d == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			d = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			d = 0;
+		}
+		a++;
+	}
+
+	if (d == 0)
+		return (0);
+
+	return (c);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, x, y, z;
+
+	sum = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] > '9' || argv[x][y] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (z = 1; z < argc; z++)
+	{
+		num = _atoi(argv[z]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
