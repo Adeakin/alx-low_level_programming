e5fe811cf8682bf2cac7637b6f75ff9baad9f9ed
Adeakin <abimbolaakinshilo@gmail.com>
2023-10-27 19:27:24 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
