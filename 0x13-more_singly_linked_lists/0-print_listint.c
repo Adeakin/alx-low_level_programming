0f48ac002711d201a9ded299a2e8ddd2cce4692a
Adeakin <abimbolaakinshilo@gmail.com>
2023-11-19 22:53:38 -0800

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a linked list
+ * @h: linked list to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+More singly linked lists
+
+Learning Objectives
+At the end of this project, the aims below were achieved:
+
+General
+How to use linked lists
+Start to look for the right source of information without too much help
