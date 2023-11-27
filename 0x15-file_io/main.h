46943218b6b0f74c347f573360dbe2b83eee276b
Adeakin <abimbolaakinshilo@gmail.com>
2023-11-27 15:33:36 -0800

    task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: holberton.h
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
