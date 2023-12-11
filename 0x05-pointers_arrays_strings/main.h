8252307118575c2d21f491ef0e0128d327e5c7ee
Adeakin <abimbolaakinshilo@gmail.com>
2023-12-10 23:53:50 -0800

    pointers_arrays completion

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ void rev_string(char *s);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
 
 
 #endif
