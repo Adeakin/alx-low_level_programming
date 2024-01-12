cafb4941c70aa82733aee0af5aa26613a4241933
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-11 18:37:11 -0800

    0x17-doubly_linked_lists-task 0-2

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ * of a slistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int tally;
+
+	tally = 0;
+
+	if (h == NULL)
+		return (tally);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		tally++;
+		h = h->next;
+	}
+
+	return (tally);
+}
