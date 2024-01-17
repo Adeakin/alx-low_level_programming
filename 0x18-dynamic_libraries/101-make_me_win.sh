d8c6a6e43cfbf2292f87a6cdc26bc7aedc058091
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-17 09:58:29 -0800

    advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Adeakin/alx-low_level_programming/main/0x18-dynamic_libraries/libhackit.so
-export LD_PRELOAD="$PWD/../libhackit.so"
+wget -P .. https://raw.githubusercontent.com/Adeakin/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
